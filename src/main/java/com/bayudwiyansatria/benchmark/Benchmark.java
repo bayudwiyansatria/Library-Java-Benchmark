f3354ec6db136e6cc77f0f3f321de51c59129a27
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2019-10-26 22:23:22 +0700

    Disable Target

--- src/main/java/com/bayudwiyansatria/benchmark/Benchmark.java
+++ src/main/java/com/bayudwiyansatria/benchmark/Benchmark.java
@@ -1,4 +1,16 @@
 package com.bayudwiyansatria.benchmark;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 public class Benchmark {
+	public static void main (String[]args){
+	
+	}
 }
--- src/main/java/com/bayudwiyansatria/benchmark/ParallelBenchmark.java
+++ src/main/java/com/bayudwiyansatria/benchmark/ParallelBenchmark.java
@@ -1,4 +1,28 @@
 package com.bayudwiyansatria.benchmark;
 
+import java.util.stream.Stream;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 public class ParallelBenchmark {
+	public void measureProcessing(Stream<String> input) {
+		final long start = System.nanoTime();
+		try (Stream<String> lines = input) {
+			final double cpuTime = lines.parallel().mapToLong(ParallelBenchmark::processLine).sum(), realTime = System.nanoTime() - start;
+			final int cores = Runtime.getRuntime().availableProcessors();
+			System.out.println("          Cores: " + cores);
+			System.out.format("       CPU time: %.2f s\n", cpuTime / SECONDS.toNanos(1));
+			System.out.format("      Real time: %.2f s\n", realTime / SECONDS.toNanos(1));
+			System.out.format("CPU utilization: %.2f%%\n\n", 100.0 * cpuTime / realTime / cores);
+		}
+	}
+	
+	public static long processLine(String line) {
+		final long localStart = System.nanoTime();
+		for (int i = 0; i < line.length(); i++) {
+			for (int j = 0; j < line.length(); j++) {
+			}
+		}
+		return System.nanoTime() - localStart;
+	}
 }
