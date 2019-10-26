7c03ecfcb56cf2e16257f124d479629ad7abe3f3
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2019-10-26 22:44:35 +0700

    Stage Data

--- src/main/java/com/bayudwiyansatria/benchmark/Benchmark.java
+++ src/main/java/com/bayudwiyansatria/benchmark/Benchmark.java
@@ -1,16 +1,11 @@
 package com.bayudwiyansatria.benchmark;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class Benchmark {
 	public static void main (String[]args){
+		double[][] data = new com.bayudwiyansatria.io.IO().readCSV_double ( "scr/main/resources/ruspini" );
 		
 	}
 }
